b6ae77ed0f6278e8cc8d74117c4065181a9d301d
RAVI KANTH VUDATHA <ravikanthvudatha@gmail.com>
2018-02-11 20:24:21 +0530

    changes in package json file

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app=express();
 var path=require('path');
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+app.engine('html', require('ejs').renderFile);
 app.use(express.static(path.join(__dirname,'dist')));
 var port=app.set('port',server_port);
 var ip=app.set('ip',server_ip_address);
